0c02a152091a30baf887c0c26bd6d4fe99885d2f
Hamed <hamedpyn@gmail.com>
2023-08-05 03:52:23 +0430

    JavaScript snippet for toggling Celsius/Fahrenheit conversions.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,19 @@
 let
     title = document.querySelector('#title h1'),
     conversionInput = document.querySelector('#conversion input'),
-    form = document.querySelector('#form'),
+    form = document.querySelector('form'),
     convertBtn = document.querySelector('#convert'),
     resetBtn = document.querySelector('#reset'),
     changeBtn = document.querySelector('#change');
-let n = 0
 //Events
-changeBtn, addEventListener('click', () => {
-
+changeBtn.addEventListener('click', ()=>{
+    if (title.innerHTML === 'Converter °C to °F') {
+        title.innerHTML = 'Converter °F to °C'
+        conversionInput.setAttribute('placeholder','°F')
+        document.title = 'Degree Conversion | °F to °C'
+    } else {
+        title.innerHTML = 'Converter °C to °F'
+        conversionInput.setAttribute('placeholder','°C')
+        document.title = 'Degree Conversion | °C to °F'
+    }
 })
