c7da8cebe2d92e1b422304489ae7587c09806817
Hamed <hamedpyn@gmail.com>
2023-08-05 03:28:49 +0430

    selection variables

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+// DOM Selectors
+
+let
+    title = document.querySelector('#title h1'),
+    conversionInput = document.querySelector('#conversion input'),
+    form = document.querySelector('#form'),
+    convertBtn = document.querySelector('#convert'),
+    resetBtn = document.querySelector('#reset'),
+    changeBtn = document.querySelector('#change');
+let n = 0
+//Events
+changeBtn, addEventListener('click', () => {
+
+})
